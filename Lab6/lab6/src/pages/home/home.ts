1ae79f258a3d20078e22771b295bd63d4ac915f4
David Saiyan <dsh9b@mail.umkc.edu>
2017-10-04 12:30:40 -0500

    lab 6 updated

--- Lab6/lab6/src/pages/home/home.ts
+++ Lab6/lab6/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private googleApiKey: string = 'AIzaSyAfNW7uuzjiuecGWV9GCSB8_Dz9J7nh2N4';
+  private query: string = 'Boogy';
+  private apiUrl: string = 'https://kgsearch.googleapis.com/v1/entities:search?query=';
+  private limit: number = 15;
+  private secondResponse: any;
+  private cxCode: string = '015955127080496721925:9hrkvdcuuuo';
+  private customSearchApi: string = 'https://www.googleapis.com/customsearch/v1?cx=' + this.cxCode + '&imgSize=icon&key=' + this.googleApiKey;
+  private response: any;
 
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+    //nothing here;
+  }
+
+
+  itemTapped(event, item) {
+    console.log(item);
+    this.imageSearch(item.result.name);
+  }
+
+  searchGoogle(): void{
+    this.http.get(this.apiUrl + this.query + '&key=' + this.googleApiKey + "&limit=" + this.limit).subscribe(data => {
+      console.log(data);
+      this.response = data;
+      this.response = this.response.itemListElement;
+    })
+  }
+
+  imageSearch(text): void{
+    this.http.get(this.customSearchApi + '&q=' + text).subscribe( data => {
+      console.log(data);
+      this.secondResponse = data;
+      this.secondResponse = this.secondResponse.items;
+    })
   }
 
 }
---
+++ Lab6/lab6/src/tests/home.test.ts
@@ -0,0 +1,27 @@
+import { HomePage} from "../pages/home/home";
+
+export class TestHome {
+
+  searchGoogleTest(): void{
+    this.httpGoogleKnowledgeMock().subscribe(data => {
+      console.log(data);
+      if(data.results.itemListElement[0].name == 'something'){
+        console.log('Test passed');
+      }else{
+        console.log('Test Failed');
+      }
+    })
+  }
+
+  httpGoogleKnowledgeMock(): any{
+    var data = {
+      results: {
+        itemListElement:[
+          {name: 'something', description: 'something'},
+          {name: 'test', description: 'test'}
+        ]
+      }
+    };
+    return data;
+  }
+}
